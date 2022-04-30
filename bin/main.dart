c645f6c56d7f80ca1f8aeb672173663c8701436c
Manuel de Jesús Gámez López <winmanuel09@gmail.com>
2022-04-30 11:50:53 -0600

    ejemplo de uso de break y continue en Dart

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  for (var i = 0; i < 10; i++) {
+    print('Número: $i');
+    //continue;
+    //break;
+    if (i == 5) {
+      break;
+    }
+    continue;
+    print('-------');
+  }
+}
---
+++ test/break_continue_test.dart
@@ -0,0 +1,8 @@
+import 'package:break_continue/break_continue.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
